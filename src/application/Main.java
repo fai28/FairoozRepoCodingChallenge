ddf8ec993c9a10bb86b4e35b6a75bb2c17f9755d
mahira.zabin1 <mahira.zabin1@IC-CS233-43.uc.ucalgary.ca>
2022-08-07 00:12:39 -0600

    Changed type of root to VBox and added a label to the window.

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,16 +3,19 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			VBox root = new VBox();
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			root.getChildren().add(new Label("Mahira's label"));
+			// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
