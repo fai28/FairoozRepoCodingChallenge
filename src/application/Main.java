74bf4c45f0e8bcf039bdc736a6978f0a0c3c446b
mahirazabin <mahirazabin@gmail.com>
2022-08-07 21:47:18 -0600

    updated title for project

--- src/application/Main.java
+++ src/application/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			root.getChildren().add(new Label("Mahira's label"));
 			root.getChildren().add(new Label("Mahira's branch"));
+			primaryStage.setTitle("Fairooz's Project");
 			// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
